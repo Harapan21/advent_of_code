f992ccfb78fec351e92055758c10e1ded647b1e5
Harapan21 <harapanpardamean@gmail.com>
2019-12-03 20:02:38 +0700

    day 2 part 1

--- day2/target/rls/debug/deps/day2-010820c25b116a93.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Data/coding/advent_of_code/day2/target/rls/debug/deps/day2-010820c25b116a93.rmeta: day2/src/main.rs
-
-/Volumes/Data/coding/advent_of_code/day2/target/rls/debug/deps/day2-010820c25b116a93.d: day2/src/main.rs
-
-day2/src/main.rs:
